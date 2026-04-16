3b125441784215d558c77c85d7c73e8754591ac3
Felipe Schmitt <schmittfel23@gmail.com>
2026-04-16 13:16:04 -0300

    added initial logic to get the match session objects from the database and build on the frontend

--- frontend/app/game/page.tsx
+++ frontend/app/game/page.tsx
@@ -8,31 +8,42 @@ import { io } from "socket.io-client"
 
 export default function GameScreen() {
     useEffect(() => {
-        // const socket = io('http://localhost:3001')
+        const socket = io('http://localhost:3001')
 
-        // socket.on('chat', (message) => {
-        //     console.log(`Mensagem recebida do backend: ${message}`)
-        //     const chat: any = document.getElementById('test-chat')
-        //     chat.innerHTML += `<p><span class="text-[${message.color}] font-bold mb-2">${message.sender}:</span>${message.text}</p>`
-        // })
+        socket.on('chat', (message) => {
+            console.log(`Message from backend: ${message}`)
+            const chat: any = document.getElementById('test-chat')
+            chat.innerHTML += `<p class="mb-2"><span class="text-[${message.color}] font-bold">${message.sender}:</span>${message.text}</p>`
+        })
 
-        // function sendChatMessage() {
-        //     const input = document.getElementById('message-input') as HTMLInputElement
+        const button = document.getElementById('send-button') as HTMLButtonElement
+        
+        function sendChatMessage() {
+            const input = document.getElementById('message-input') as HTMLInputElement
+            
+            socket.emit('chat', { sender: 'User', text: input.value })
+            
+            input.value = ''
+        }
 
-        //     socket.emit('chat', { sender: 'Usuário', text: input.value })
+    
+        
+        
+        
 
-        //     input.value = ''
-        // }
+        socket.emit('build_scene', 'just to get the objects of the scene from the database')
 
-
-
-
-
-
+        socket.on('scene_add_objects', (objectsList) => {
+            console.log(objectsList)
+        })
+        
+        
         const gameScreen = document.getElementById('gamescreen') as HTMLCanvasElement
         const renderer = new three.WebGLRenderer({ canvas: gameScreen })
         renderer.setPixelRatio(window.devicePixelRatio)
-        renderer.setSize(900, 600)
+        renderer.setSize(window.innerWidth, window.innerHeight)
+
+        let loop: boolean = true
 
         const scene = new three.Scene()
         const camera = new three.PerspectiveCamera(75, gameScreen.width / gameScreen.height, 1, 100)
@@ -57,31 +68,16 @@ export default function GameScreen() {
             }
         )
 
-        loader.load(
-            'models/card.glb',
-            function (gltf) {
-                scene.add(gltf.scene)
-                console.log(gltf)
-            },
-            function (progress) {
-                console.log((progress.loaded / progress.total * 100) + '% loaded')
-            },
-            function (error) {
-                console.error(error)
-                alert(`Deu ruim no modelo: ${error}`)
-            }
-        )
-
-        let loop: boolean = true
-
-
-        camera.position.setZ(30)
+        camera.position.setX(-12)
+        camera.position.setY(28)
         light.position.set(20, 20, 20)
 
         scene.add(grid)
         scene.add(light)
 
         renderer.render(scene, camera)
+
+
 
         function gameLoop() {
             if (loop) {
@@ -91,25 +87,30 @@ export default function GameScreen() {
             }
         }
 
-        window.addEventListener('keydown', event => {
-            if (event.key == 'l') { loop ? loop = false : loop = true; gameLoop() }
-        })
+        function pauseAndResumeAnimation(event: KeyboardEvent) {
+            if (event.key == 'Enter') { loop ? loop = false : loop = true; gameLoop() }
+        }
 
+        window.addEventListener('keydown', pauseAndResumeAnimation)
+        button.addEventListener('click', sendChatMessage)
+        
         gameLoop()
-
+        
         return () => {
-            // socket.off('connection')
+            socket.off('connection')
+            window.removeEventListener('keydown', pauseAndResumeAnimation)
+            button.removeEventListener('click', sendChatMessage)
         }
     }, [])
 
     return (
         <>
-            <canvas id="gamescreen" width={900} height={600} className="border-8 border-[#b30000]"></canvas>
+            <canvas id="gamescreen"></canvas>
             <div className="absolute w-[17vw] h-[50vh] border-4 border-zinc-500 right-[80%] flex flex-col justify-evenly items-center">
                 <div id="test-chat" className="w-[90%] h-[80%] bg-[#888] flex flex-col overflow-y-scroll"></div>
 
                 <input type="text" id="message-input" className="bg-white border-[3px] border-[#4784ff] w-[90%]" />
-                <button className="bg-[#9d47ff60] border-[3px] border-[#9d47ff] rounded-[5px] w-[90%] p-0.75">MANDAR</button>
+                <button id="send-button" className="bg-[#9d47ff60] border-[3px] border-[#9d47ff] rounded-[5px] w-[90%] p-0.75">SEND</button>
             </div>
         </>
     )
